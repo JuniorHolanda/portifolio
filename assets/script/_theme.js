fc7dec4acf46c7257f67c1010b6f8c0339ee1582
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-06-09 23:27:58 -0300

    dark mode da home concluído

--- assets/script/_theme.js
+++ assets/script/_theme.js
@@ -6,26 +6,48 @@ const darkContainerObjective = document.querySelector('#darkContainerObjective')
 const darkListObjective = document.querySelector('#darkListObjective');
 const darkContainerInfoFirstPage = document.querySelector('#darkContainerInfoFirstPage');
 const darkHalftone = document.querySelector('#darkHalftone');
+const darkImgProfile = document.querySelector('#darkImgProfile');
+const darkSobreTitle = document.querySelector('#darkSobreTitle');
+const darkSobreDescription = document.querySelector('#darkSobreDescription');
+const darkStrongDescription = document.querySelectorAll('.darkStrongDescription');
+const darkCloseTag = document.querySelector('#darkCloseTag');
+const darkMenuItemMain = document.querySelectorAll('.darkMenuItemMain');
+const darkMenuListMain = document.querySelector('#darkMenuListMain');
 //const darkContainerObjective = document.querySelector('#');
 
+// muda as imagens para o dark-theme
 function changeImg () {
     if (changeThemeBtn.checked) {
-        darkHalftone.src = 'script/dark-halftone.png'
+        darkHalftone.src = 'assets/img/dark-halftone.png'
+        darkImgProfile.src = 'assets/img/dark-profile.jpg'
     }else{
-        darkHalftone.src = 'script/halftone.png'
+        darkHalftone.src = 'assets/img/halftone.png'
+        darkImgProfile.src = 'assets/img/profile.jpg'
     }
 }
-
+//muda o tema para dark ou light
 changeThemeBtn.addEventListener('change', () => {
     //chama a função para alterar as imagens
     changeImg();
 
     containerProfile.classList.toggle('dark-container-profile-first-page');
     darkProfileBox.classList.toggle('dark-profile-box');
-    darkContainerObjective.classList.toggle('dark-container-objective')
-    darkListObjective.classList.toggle('dark-list-objective')
-    darkContainerInfoFirstPage.classList.toggle('dark-container-info-first-page')
-    //muda a img do halftone pra dark
-
+    darkContainerObjective.classList.toggle('dark-container-objective');
+    darkListObjective.classList.toggle('dark-list-objective');
+    darkContainerInfoFirstPage.classList.toggle('dark-container-info-first-page');
+    darkSobreTitle.classList.toggle('dark-sobre-title');
+    darkSobreDescription.classList.toggle('dark-sobre-description');
+    darkCloseTag.classList.toggle('dark-close-tag')
+    darkMenuListMain.classList.toggle('dark-menu-list-main')
     //darkContainerObjective.classList.toggle('')
+
+    //itera sobre a lista darkMenuMain e adiciona classe aos itens
+    darkMenuItemMain.forEach(darkMenuItemMain => {
+        darkMenuItemMain.classList.toggle('dark-menu-item-main') ; 
+    });
+
+    //itera sobre a lista darkStrongDescription e adiciona classe aos itens
+    darkStrongDescription.forEach(darkStrongDescription => {
+        darkStrongDescription.classList.toggle('dark-strong-description') ; 
+    });
 });
