f85211a14e07c2f5ab1b1718ea02878917f2419d
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-07-30 19:09:41 -0300

    testes para o tootip

--- assets/script/tooltip.js
+++ assets/script/tooltip.js
@@ -2,13 +2,12 @@ export const toolTip = document.addEventListener('DOMContentLoaded', function ()
     const toolTipsElements = document.querySelectorAll('.tooltip-target');
 
     toolTipsElements.forEach(element => {
+        
         const toolTipText = element.getAttribute('data-tooltip');
         const toolTip = document.createElement('div');
         toolTip.classList.add('tooltip');
         toolTip.innerText = toolTipText;
-        console.log("vai adicionar o elemento no dom")
         element.appendChild(toolTip);
-        console.log("adc o elemento no dom")
 
         element.addEventListener('mouseenter', () => {
             toolTip.classList.add('tooltip-visible');
@@ -17,7 +16,7 @@ export const toolTip = document.addEventListener('DOMContentLoaded', function ()
 
         element.addEventListener('mouseleave', () => {
             toolTip.classList.remove('tooltip-visible');
-            console.log('até aqui remove');
+
         });
     });
 });
