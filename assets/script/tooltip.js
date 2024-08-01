248e5ab2c4cd1bf389a1515c0a10ce333fd1d91f
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-07-31 21:38:40 -0300

    tooltip finalizada
    tive dificuldade para saber como colocar o tooltip sobre o ícone que ela representa, o gpt tava me sugerindo uma forma absurda para posicionar utilizando inclusive práticas já depreciadas pela comunidade, na verdade precisei apenas fazer alguns ajustes no html que deu certinho.

--- assets/script/tooltip.js
+++ assets/script/tooltip.js
@@ -3,29 +3,37 @@ export const toolTip = document.addEventListener('DOMContentLoaded', function ()
 
     toolTipsElements.forEach(element => {
         //cria o título da tooltip
-        const toolTipTitle = element.getAttribute('data-title-tooltip')
-        const title = document.createElement('h3')
-        title.innerText = toolTipTitle
-        title.classList.add('tooltip-title')
+        const toolTipTitle = element.getAttribute('data-title-tooltip');
+        const title = document.createElement('h3');
+        title.innerText = toolTipTitle;
+        title.classList.add('tooltip-title');
+
+        //cria o separador
+        const separador = document.createElement('hr');
+        separador.classList.add('separador-tooltip');
 
         //cria o texto da tooltip
         const toolTipText = element.getAttribute('data-tooltip');
-        const toolTip = document.createElement('div');
+        const toolTip = document.createElement('p');
         toolTip.classList.add('tooltip');
         toolTip.innerText = toolTipText;
 
-        element.parentElement.appendChild(title);
-        element.parentElement.appendChild(toolTip);
+        //cria o container para o tooltip
+        const containerToolTip = document.createElement('div');
+        containerToolTip.classList.add('container-tooltip');
+        containerToolTip.appendChild(title);
+        containerToolTip.appendChild(separador);
+        containerToolTip.appendChild(toolTip);
+
+        //adiciona o container toolTip ao elemento da lista
+        element.parentElement.appendChild(containerToolTip);
 
         element.addEventListener('mouseenter', () => {
-            toolTip.classList.add('tooltip-visible');
-            title.classList.add('title-tooltip-visible');
-            console.log(toolTipText);
+            containerToolTip.classList.add('visible-container-tooltip');
         });
 
         element.addEventListener('mouseleave', () => {
-            toolTip.classList.remove('tooltip-visible');
-            title.classList.remove('title-tooltip-visible');
+            containerToolTip.classList.remove('visible-container-tooltip');
 
         });
     });
